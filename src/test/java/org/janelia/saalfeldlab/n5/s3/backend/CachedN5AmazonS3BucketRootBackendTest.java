6d8567a5af570dcd922753b1716c524bd18424df
Caleb Hulbert <cmhulbert@gmail.com>
2023-06-15 15:21:20 -0400

    feat(test): cache tests

---
+++ src/test/java/org/janelia/saalfeldlab/n5/s3/backend/CachedN5AmazonS3BucketRootBackendTest.java
@@ -0,0 +1,62 @@
+/*-
+ * #%L
+ * N5 AWS S3
+ * %%
+ * Copyright (C) 2017 - 2022, Saalfeld Lab
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package org.janelia.saalfeldlab.n5.s3.backend;
+
+import com.google.gson.GsonBuilder;
+import org.janelia.saalfeldlab.n5.N5Writer;
+import org.janelia.saalfeldlab.n5.s3.AbstractN5AmazonS3BucketRootTest;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Initiates testing of the Amazon Web Services S3-based N5 implementation using actual S3 backend.
+ * The test N5 container is created at the root of the new temporary bucket.
+ *
+ * @author Igor Pisarev &lt;pisarevi@janelia.hhmi.org&gt;
+ */
+public class CachedN5AmazonS3BucketRootBackendTest extends AbstractN5AmazonS3BucketRootTest {
+
+    public CachedN5AmazonS3BucketRootBackendTest() {
+
+        super(BackendS3Factory.getOrCreateS3());
+    }
+
+	@Override
+	protected N5Writer createN5Writer(final String location, final GsonBuilder gson) throws IOException, URISyntaxException {
+
+		final URI uri = new URI(location);
+		final String bucketName = uri.getHost();
+
+		N5AmazonS3DelayedWriter.sleep();
+		return new N5AmazonS3DelayedWriter(s3, bucketName, gson, true);
+	}
+
+}

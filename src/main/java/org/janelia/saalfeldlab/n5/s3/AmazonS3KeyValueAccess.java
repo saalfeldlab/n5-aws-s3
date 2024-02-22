d12b257e1dd6ab3d7fafc159e9f06a76bd0de7a2
Caleb Hulbert <cmhulbert@gmail.com>
2024-02-22 16:32:43 -0500

    fix: `KeyValueAccess#components(String)` should return input if only one component

--- src/main/java/org/janelia/saalfeldlab/n5/s3/AmazonS3KeyValueAccess.java
+++ src/main/java/org/janelia/saalfeldlab/n5/s3/AmazonS3KeyValueAccess.java
@@ -110,7 +110,10 @@ public class AmazonS3KeyValueAccess implements KeyValueAccess {
 	@Override
 	public String[] components(final String path) {
 
-		return Arrays.stream(path.split("/"))
+		final String[] baseComponents = path.split("/");
+		if (baseComponents.length <= 1)
+			return baseComponents;
+		return Arrays.stream(baseComponents)
 				.filter(x -> !x.isEmpty())
 				.toArray(String[]::new);
 	}

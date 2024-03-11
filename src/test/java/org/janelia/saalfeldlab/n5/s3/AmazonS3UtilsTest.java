4843fde27a0347d8a7e4013c6dd38fda07934106
Caleb Hulbert <cmhulbert@gmail.com>
2024-03-11 11:40:53 -0400

    feat(test): add test for AmazonS3Utils#getS3Bucket/getS3Key

--- src/test/java/org/janelia/saalfeldlab/n5/s3/AmazonS3UtilsTest.java
+++ src/test/java/org/janelia/saalfeldlab/n5/s3/AmazonS3UtilsTest.java
@@ -1,10 +1,14 @@
 package org.janelia.saalfeldlab.n5.s3;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.google.common.collect.Maps;
 import org.junit.Test;
 
 public class AmazonS3UtilsTest {
@@ -36,6 +40,61 @@ public class AmazonS3UtilsTest {
 					assertEquals("key from uri", path, AmazonS3Utils.getS3Key(uri));
 				}
 
+	}
+
+	@Test
+	public void getS3InfoTest() {
+
+		final String bucketName = "my-s3-bucket";
+		final String[] noKeyTests = new String[]{
+				"https://test.com/" + bucketName,
+				"http://test.com/" + bucketName + "/",
+				"s3://" + bucketName,
+				"s3://" + bucketName + "/",
+
+		};
+
+
+		final String[] onePartKeyWithSlashTests = new String[]{
+				"https://test.com/" + bucketName + "/a/",
+				"s3://" + bucketName + "/a/",
+		};
+
+		final String[] multiPartKeyWithSlashTests = new String[]{
+				"https://test.com/" + bucketName + "/a/b/c/d/",
+				"s3://" + bucketName + "/a/b/c/d/",
+		};
+
+
+
+		final String[] onePartKeyNoSlashTests = new String[]{
+				"https://test.com/" + bucketName + "/a",
+				"s3://" + bucketName + "/a",
+		};
+
+		final String[] multiPartKeyNoSlashTests = new String[]{
+				"https://test.com/" + bucketName + "/a/b/c/d",
+				"s3://" + bucketName + "/a/b/c/d",
+		};
+
+		final HashMap<String, String[]> keyToTests = new HashMap<>();
+
+		keyToTests.put("", noKeyTests);
+		keyToTests.put("a/", onePartKeyWithSlashTests);
+		keyToTests.put("a/b/c/d/", multiPartKeyWithSlashTests);
+		keyToTests.put("a", onePartKeyNoSlashTests);
+		keyToTests.put("a/b/c/d", multiPartKeyNoSlashTests);
+
+		for (Map.Entry<String, String[]> tests : keyToTests.entrySet()) {
+			final String expectedKey = tests.getKey();
+			for (String uri : tests.getValue()) {
+				assertEquals(bucketName, AmazonS3Utils.getS3Bucket(uri));
+				assertEquals("Unexpected key for " + uri, expectedKey, AmazonS3Utils.getS3Key(uri));
+			}
+		}
+
+		assertNull("Invalid URI should return null for bucket", AmazonS3Utils.getS3Bucket("invalid uri \\ _ ~ 435:  q2234[;5."));
+		assertEquals("Invalid URI returns empty string for key", "", AmazonS3Utils.getS3Key("invalid uri \\ _ ~ 435:  q2234[;5."));
 	}
 
 }

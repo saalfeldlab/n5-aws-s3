69c76738cd8a949ac03466da46767e743f7e3139
John Bogovic <bogovicj@janelia.hhmi.org>
2025-05-12 16:43:30 -0400

    test: toward having the tests start the minio server

--- src/test/java/org/janelia/saalfeldlab/n5/s3/N5AmazonS3MockTests.java
+++ src/test/java/org/janelia/saalfeldlab/n5/s3/N5AmazonS3MockTests.java
@@ -1,16 +1,44 @@
 package org.janelia.saalfeldlab.n5.s3;
 
+import java.net.URI;
+import java.nio.file.Path;
+
 import org.janelia.saalfeldlab.n5.s3.mock.MockS3Factory;
+import org.janelia.saalfeldlab.n5.s3.mock.RunnerWithMinioServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+
 import software.amazon.awssdk.services.s3.S3Client;
 
 public class N5AmazonS3MockTests extends N5AmazonS3Tests {
 
+	public Path minioServerDirectory;
+
+	public URI minioUri;
+
+	public RunnerWithMinioServer runner;
 
 	static {
 		/* Should be no Erroneous Backend Failures with Mock Backend */
 		skipErroneousBackendFailures = false;
+	}
+
+	@Before
+	public void startServer() throws Exception {
+
+		runner = new RunnerWithMinioServer(getClass());
+		runner.startMinioServer();
+
+		this.minioServerDirectory = runner.minioServerDirectory;
+		this.minioUri = runner.minioUri;
+	}
+
+	@After
+	public void stopServer() {
+
+		runner.stopMinioServer();
 	}
 
 	@Override
